e53098463f7c13146fff56deb1854188f1ae9271
komlan akakpo <kakakpo2@unl.edu>
2020-08-25 01:47:55 +0000

    added major

--- hello.c
+++ hello.c
@@ -1,15 +1,15 @@
 /**
  * Author: Komlan Akakpo
  * Date:   2020
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    
+
     printf("hello komlan akakpo!\n");
-    
+    printf("Electrical Engineering\n");
     return 0;
 }
