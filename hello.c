d9acd215bf3d19a690cd8087bdbc514d7021358f
komlan akakpo <kakakpo2@unl.edu>
2020-08-25 01:23:20 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Komlan Akakpo
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("hello komlan akakpo!\n");
+    
+    return 0;
+}
